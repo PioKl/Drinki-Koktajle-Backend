10cae86939c398044afe2b54aa5487d853caf9de
PioKl <kosiu342@gmail.com>
2021-06-02 19:18:07 +0200

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0a5b204a257872a83b3b43a2946a521a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '146573c9-c989-43b4-8b84-871950025b1a'
+};
