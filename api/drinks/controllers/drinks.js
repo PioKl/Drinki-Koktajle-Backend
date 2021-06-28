8e4476e50439d2a37b997eb9d830f0e5609562c1
PioKl <kosiu342@gmail.com>
2021-06-28 12:46:21 +0200

    Adding custom user endpoints to get all drinks logged in user

--- api/drinks/controllers/drinks.js
+++ api/drinks/controllers/drinks.js
@@ -1,8 +1,32 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    //Pobranie zalogowanych użytkowników
+    async me(ctx) {
+        const user = ctx.state.user //pobranie użytkownika (będzie wymagany do tego token)
+
+        //Jeśli nie ma takiego użytkownika, czyli token nie będzie się w przyszłości zgadzał
+        if(!user) {
+            return ctx.badRequest(null, [
+                {messages: [{id: "No authorization header was found"}]},
+            ]);
+        }
+
+        //tutaj będą znajdowały się wszystkie drinki jakie zalogowany użytkownik posiada
+        const data = await strapi.services.drinks.find({user: user.id})
+
+        //jeśli nie znalazło drinków, które ma użytkownik
+        if(!data) {
+            return ctx.notFound();
+        }
+        //zwrócenie tych danych
+        return sanitizeEntity(data, {model: strapi.models.drinks});
+
+    },
+};
