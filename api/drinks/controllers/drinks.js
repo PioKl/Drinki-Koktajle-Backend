3d8cac70ce0d739a3faafdaadc202357f539fad6
PioKl <kosiu342@gmail.com>
2021-06-28 13:37:04 +0200

    Adding Owner Policy for drinks

--- api/drinks/controllers/drinks.js
+++ api/drinks/controllers/drinks.js
@@ -7,6 +7,81 @@ const { sanitizeEntity } = require('strapi-utils');
  */
 
 module.exports = {
+
+    //Dodanie Owner Policy w celu zabezpieczenia przed edytowaniem i usunięciem drinków, które nie należą do danego użytkownika
+    //https://strapi.io/documentation/developer-docs/latest/guides/is-owner.html#apply-the-author-by-default
+
+    //Połączenie użytkownika z danym drinkiem (z drinkiem, który stworzył i należy do niego)
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+        const { data, files } = parseMultipartData(ctx);
+        data.user = ctx.state.user.id;
+        entity = await strapi.services.drinks.create(data, { files });
+        } else {
+        ctx.request.body.user = ctx.state.user.id;
+        entity = await strapi.services.drinks.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.drinks });
+    },
+
+    //Aktualizacja drinka należącego tylko do użytkownika, który stworzył dany drink
+    async update(ctx) {
+        const { id } = ctx.params;
+    
+        let entity;
+    
+        const [drinks] = await strapi.services.drinks.find({
+          id: ctx.params.id,
+          'user.id': ctx.state.user.id,
+        });
+    
+        if (!drinks) {
+          return ctx.unauthorized(`You can't update this entry`);
+        }
+    
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.drinks.update({ id }, data, {
+            files,
+          });
+        } else {
+          entity = await strapi.services.drinks.update({ id }, ctx.request.body);
+        }
+    
+        return sanitizeEntity(entity, { model: strapi.models.drinks });
+      },
+
+      //Usunięcie drinka należącego tylko do użytkownika, który go stworzył
+
+      async delete(ctx) {
+        const { id } = ctx.params;
+    
+        let entity;
+    
+        const [drinks] = await strapi.services.drinks.find({
+          id: ctx.params.id,
+          'user.id': ctx.state.user.id,
+        });
+    
+        if (!drinks) {
+          return ctx.unauthorized(`You can't update this entry`);
+        }
+    
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.drinks.update({ id }, data, {
+            files,
+          });
+        } else {
+          entity = await strapi.services.drinks.update({ id }, ctx.request.body);
+        }
+    
+        return sanitizeEntity(entity, { model: strapi.models.drinks });
+      },
+
+    //Koniec Owner Policy
+
     //Pobranie zalogowanych użytkowników
     async me(ctx) {
         const user = ctx.state.user //pobranie użytkownika (będzie wymagany do tego token)
